a706d81a1909170fe93fb0261b5f155c929910ed
Omar Maroki <omar987@hotmail.co.uk>
2022-06-11 16:39:49 +0100

    random Pokemon id fetcher

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
 
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center ">
-			<div className="bg-red-200 text-center">Which pokemon is rounder</div>
+			<div className=" text-center">Which pokemon is rounder</div>
 			<div className="p-2" />
 			<div className="border rounded p-8 flex justify-between max-w-2xl items-center">
 				<div className="w-16 h-16 bg-red-800">{first}</div>
