48beaa13ee3b1041ddc66eb6f0a1a30d28b41cb0
Omar Maroki <omar987@hotmail.co.uk>
2022-06-11 16:28:36 +0100

    csr setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,20 @@
+import { getOptionsForVote } from '@/utils/getRandomPokemon';
 import { trpc } from '@/utils/trpc';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 
 const Home: NextPage = () => {
-	const { data, isLoading } = trpc.useQuery(['hello', { text: 'Omar' }]);
-
-	if (isLoading) return <div>Loading...</div>;
-	if (data) return <div>{data.greeting}</div>;
+	const [first, second] = getOptionsForVote();
 
 	return (
 		<div className="h-screen w-screen flex flex-col justify-center items-center ">
 			<div className="bg-red-200 text-center">Which pokemon is rounder</div>
 			<div className="p-2" />
 			<div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-				<div className="w-16 h-16 bg-red-200 "></div>
+				<div className="w-16 h-16 bg-red-800">{first}</div>
 				<div className="p-8">Vs</div>
-				<div className="w-16 h-16 bg-red-200 "></div>
+				<div className="w-16 h-16 bg-red-800">{second}</div>
 			</div>
 		</div>
 	);
